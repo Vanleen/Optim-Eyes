cbf086d21c7fbd5a1ad874d306bc82a24dd8a471
Vanilda Vasconcelos <vanilda.vas@gmail.com>
2025-02-02 05:10:12 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white text-3xl p-4">
+      🎉 Optim’eyes fonctionne avec Tailwind CSS !
+    </div>
+  );
+}
+
+export default App;
