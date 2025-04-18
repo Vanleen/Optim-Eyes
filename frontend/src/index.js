6aac6d6c683aecf6061b62ab024fc66a4b2a06f5
Vanilda Vasconcelos <vanilda.vas@gmail.com>
2025-04-18 12:04:17 +0200

    Ajout du modèle Preference pour les recommandations

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,11 @@
+// frontend/src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+
+// ✨ Votre log juste avant le montage
+console.log('🚀 React démarre bien…');
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -9,3 +13,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
